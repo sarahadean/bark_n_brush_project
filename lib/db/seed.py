f1e7927ab9f6c6a5259ddcc9c8c0126b0297de3d
Matt Brouwer <notjonbonjovi@gmail.com>
2023-06-06 13:36:58 -0400

    troubleshooting

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,26 +1,2 @@
-from datetime import datetime
-# from faker import Faker
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from models import *
-
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///barknbrush.db')
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
     
-    dog1  = Dog('Tucker', 'Labradoodle', 3)
-    dog2 = Dog('Doug', 'Pug', 5)
-    dog3 = Dog('Thor', 'Husky', 7)
-
-    owner1 = Owner('Matt')
-    owner2 = Owner('Matty')
-    owner3 = Owner('Sarah')
-
-    
-
-
-    session.commit()
-    session.close()
