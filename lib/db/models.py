99a40cf6ea28d9fe328d91b26f48ecb5c22bcd7d
Matt Brouwer <notjonbonjovi@gmail.com>
2023-06-06 11:14:38 -0400

    __repr__ added

--- lib/db/models.py
+++ lib/db/models.py
@@ -5,10 +5,8 @@ from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///barknbrush.db')
-    Base = declarative_base()
-
+engine = create_engine('sqlite:///barknbrush.db')
+Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
@@ -18,12 +16,22 @@ class Dog(Base):
     breed = Column(String())
     age = Column(Integer())
 
+    def __repr__(self):
+        return f"Dog ID {self.id}:" \
+            + f" Name: {self.name}," \
+            + f" Breed: {self.breed}," \
+            + f" Age: {self.age}," 
+
 
 class Owner(Base):
     __tablename__ = 'owners'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
+
+    def __repr__(self):
+        return f"Owner ID {self.id}:" \
+            + f" Name: {self.name},"
 
 
 class Appointment(Base):
@@ -33,8 +41,15 @@ class Appointment(Base):
     date_and_time = Column(DateTime())
     service = Column(String())
     price = Column(Integer())
-    dog_id = Column(Integer, ForeignKey('dogs.id'))
+    dog_id = Column(Integer(), ForeignKey('dogs.id'))
     owner_id = Column(Integer(), ForeignKey('owners.id'))
 
     dog = relationship('Dog', backref='dogs')
     owner = relationship('Owner', backref='owners')
+
+    def __repr__(self):
+        return f"Appointment ID {self.id}:" \
+            + f" Date and Time: {self.date_and_time,}" \
+            + f"Service: {self.service}," \
+            + f"Price: {self.price}," 
+            
