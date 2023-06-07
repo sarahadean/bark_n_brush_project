2836488d35017c2a2005b5ee7e0d8ae93181c69e
Matthew Phan <mttphan261@gmail.com>
2023-06-06 21:55:41 -0500

    relationships complete

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 
 from sqlalchemy import create_engine, desc, func
-from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
 engine = create_engine('sqlite:///barknbrush.db')
 Base = declarative_base()
+
+appointment_user = Table(
+    'appointment_users',
+    Base.metadata,
+    #Column('attribute name', ForeignKey(table.id))
+    Column('dog_id', ForeignKey('dogs.id'), primary_key=True),
+    Column('owner_id', ForeignKey('owners.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Dog(Base):
     __tablename__ = 'dogs'
@@ -16,17 +25,20 @@ class Dog(Base):
     breed = Column(String())
     age = Column(Integer())
 
-    owner_id = Column(Integer(), ForeignKey('owners.id'))
-    # owner = Column(String(), ForeignKey('owners.name')) #remove later 
-    owner = relationship('Owner', back_populates='best_friend')
+    # owner_id = Column(Integer(), ForeignKey('owners.id'))
+    # owner = relationship('Owner', back_populates='best_friend')
+
+    #column = relationship('Class_we're_pointing_to', back_populates='column_in_class_referred')
+    owners = relationship('Owner', secondary=appointment_user, back_populates='best_friends')
+    appointments = relationship('Appointment', backref=backref('dog'), cascade='all, delete-orphan')
 
     def __repr__(self):
-        return f"Dog ID {self.id}:" \
+        return f"Dog ID {self.id}," \
             + f" Name: {self.name}," \
             + f" Breed: {self.breed}," \
             + f" Age: {self.age}," \
-            + f" Owner: {self.owner}," \
-            + f" Owner ID: {self.owner_id},"
+            # + f" Owner: {self.owner}," \
+            # + f" Owner ID: {self.owner_id},"
 
 
 class Owner(Base):
@@ -34,7 +46,11 @@ class Owner(Base):
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
-    best_friend = relationship('Dog', back_populates='owner')
+
+    # best_friend = relationship('Dog', back_populates='owner')
+
+    best_friends = relationship('Dog', secondary=appointment_user, back_populates='owners')
+    appointments = relationship('Appointment', backref=backref('owner'), cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"Owner ID {self.id}:" \
@@ -45,13 +61,19 @@ class Appointment(Base):
     __tablename__ = 'appointments'
 
     id = Column(Integer(), primary_key=True)
-    date_and_time = Column(DateTime())
+    date_and_time = Column(DateTime(), server_default=func.now())
     service = Column(String())
     price = Column(Integer())
     
+    dog_id = Column(Integer(), ForeignKey('dogs.id'))
+    owner_id = Column(Integer(), ForeignKey('owners.id'))
+
+    # best_friends = relationship('Dog', secondary=appointment_user, back_populates='appointments')
+    # owners = relationship('Owner', secondary=appointment_user, back_populates='best_friends')
     
     def __repr__(self):
         return f"Appointment ID {self.id}:" \
             + f" Date and Time: {self.date_and_time,}" \
             + f" Service: {self.service}," \
-            + f" Price: {self.price},"
+            + f" Price: {self.price}," \
+            
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import *
+from models import Dog, Owner, Appointment
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///barknbrush.db')
@@ -13,17 +13,50 @@ if __name__ == '__main__':
     session.query(Dog).delete()
     session.query(Owner).delete()
 
+    #Appoinment: 
+    # date_and_time
+    # service
+    # price
+    # dog_id
+    # owner_id
+    # best_friends
+    # owners
+
+    #Dog:
+    # name
+    # breed
+    # age
+
+
     
     owner1 = Owner(name = 'Matt')
     owner2 = Owner(name = 'Matty')
     owner3 = Owner(name = 'Sarah')
 
-    dog1  = Dog(name = 'Tucker', breed = 'Labradoodle', age = 3, owner_id = 1)
-    dog2 = Dog(name = 'Doug', breed = 'Pug', age = 5, owner_id = 2)
-    dog3 = Dog(name = 'Thor', breed = 'Husky', age = 7,  owner_id = 3)
+    dog1  = Dog(name = 'Tucker', breed = 'Labradoodle', age = 3)
+    dog2 = Dog(name = 'Doug', breed = 'Pug', age = 5)
+    dog3 = Dog(name = 'Thor', breed = 'Husky', age = 7)
 
-
-
-    session.add_all([dog1, dog2, dog3, owner1, owner2, owner3])
+#Teddy Code
+    appointment1 = Appointment(date_and_time=datetime.now(), service='Grooming', price=50)
+    appointment2 = Appointment(date_and_time=datetime.now(), service='Walk', price=25)
+    
+    # Associate dogs and owners with appointments
+    dog1.appointments.append(appointment1)
+    dog2.appointments.append(appointment1)
+    dog2.appointments.append(appointment2)
+    dog3.appointments.append(appointment2)
+    
+    owner1.appointments.append(appointment1)
+    owner2.appointments.append(appointment1)
+    owner2.appointments.append(appointment2)
+    owner3.appointments.append(appointment2)
+    
+    session.add_all([dog1, dog2, dog3, owner1, owner2, owner3, appointment1, appointment2])
     session.commit()
     session.close()
+#Teddy Code end
+    
+    # session.add_all([dog1, dog2, dog3, owner1, owner2, owner3])
+    # session.commit()
+    # session.close()
